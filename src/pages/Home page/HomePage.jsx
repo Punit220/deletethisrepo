68344ca7828658866ae81ba0a2f3996c78ca1c12
Punit220 <tandelpunit22@gmail.com>
2024-10-23 17:25:12 +0530

    changes in news page

--- src/pages/Home page/HomePage.jsx
+++ src/pages/Home page/HomePage.jsx
@@ -1,18 +1,22 @@
-import './style.css'
-import React from 'react'
+import "./style.css";
+import React from "react";
 
 const HomePage = () => {
   return (
     <div>
-    <div class="container">
-    <div class="content">
-        <h2>About Us</h2>
-        <p>This is a simple homepage template. You can customize it further by adding more sections, changing colors, or integrating it into your project.</p>
-        <p>Feel free to replace this content with your own information!</p>
+      <div class="container">
+        <div class="content">
+          <h2>About Us</h2>
+          <p>
+            This is a simple homepage template. You can customize it further by
+            adding more sections, changing colors, or integrating it into your
+            project.
+          </p>
+          <p>Feel free to replace this content with your own information!</p>
+        </div>
+      </div>
     </div>
-</div>
-    </div>
-  )
-}
+  );
+};
 
-export default HomePage
+export default HomePage;
--- src/pages/newspage/NewsPage.jsx
+++ src/pages/newspage/NewsPage.jsx
@@ -1,6 +1,18 @@
 
 export const NewsPage = () => {
   return (
-    <div>NewsPage</div>
+    <div>
+      <div class="container">
+        <div class="content">
+          <h2>news page</h2>
+          <p>
+            This is a simple newspage template. You can customize it further by
+            adding more sections, changing colors, or integrating it into your
+            project.
+          </p>
+          <p>Feel free to replace this cgiontent with your own information!</p>
+        </div>
+      </div>
+    </div>
   )
 }
