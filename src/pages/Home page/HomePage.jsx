4f70b67caf353f117ae096a3538fdeeeaeca168a
Punit220 <tandelpunit22@gmail.com>
2024-10-23 17:12:21 +0530

    changes in home page

--- src/pages/Home page/HomePage.jsx
+++ src/pages/Home page/HomePage.jsx
@@ -1,8 +1,17 @@
+import './style.css'
 import React from 'react'
 
 const HomePage = () => {
   return (
-    <div>HomePage</div>
+    <div>
+    <div class="container">
+    <div class="content">
+        <h2>About Us</h2>
+        <p>This is a simple homepage template. You can customize it further by adding more sections, changing colors, or integrating it into your project.</p>
+        <p>Feel free to replace this content with your own information!</p>
+    </div>
+</div>
+    </div>
   )
 }
 
